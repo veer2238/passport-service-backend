1dbe8bb0e73096e8bcaa980d2c768c29cc67c545
veer2238 <veer2238rajput@gmail.com>
2023-12-11 00:34:16 +0530

    vercel config

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 import express from "express";
+import cors from "cors"
+import bodyParser from "body-parser";
+import nodemailer from "nodemailer"
 
 const app = express()
 
-app.get('/',(req,res) =>{
-    res.json({message:"hello express"})
-})
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/', async (req, res) => {
+    try {
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'veer2238rajput@gmail.com',
+          pass: 'jeasqtbfommzmxya'
+        },
+      });
+  
+    
+  
+      let info = await transporter.sendMail({
+        from: 'veer2238rajput@gmail.com',
+        to: 'himanshu0409agraval@gmail.com',
+        subject: "thanks for registration",
+        text: "Hello world?",
+        html: "my name is " +req.body.name + "<br>" +"i attend "+ req.body.date +" lecture"+ "<br>" +"my work:"+ req.body.work
+      });
+  
+      console.log("Message sent: %s", info.messageId);
+  
+     // Respond with success
+     
+     res.status(200).json({ success: true, message: 'Email sent successfully' });
+    } catch (error) {
+      console.error('Attendance Error:', error);
+      res.status(500).json({ success: false, error: 'Failed to send email' });
+    }
+  });
 
 
 const port =9000
