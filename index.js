8bfe4ddb16ef461477fcc0e8573a02b2f3921f20
veer2238 <veer2238rajput@gmail.com>
2023-12-12 13:52:26 +0530

    index config

--- index.js
+++ index.js
@@ -2,57 +2,102 @@ import express from "express";
 import cors from "cors"
 import bodyParser from "body-parser";
 import nodemailer from "nodemailer"
+import mongoose  from "mongoose";
 
 const app = express()
+// Replace the following with your Atlas connection string                                                                                                                                        
+mongoose.connect("mongodb+srv://veer2238rajput:STrgrNlEXyfMZHBs@cluster0.3chkue4.mongodb.net/Contact?retryWrites=true&w=majority")
+.then(() => console.log("mongodb connected"))
+.catch(err => console.log("mongo error",err))
+
+//create schemaa 
+const attendaceSchema = new mongoose.Schema({ 
+  name: { 
+      type: String, 
+      require: true
+  }, 
+  date: { 
+      type: String, 
+      require: true
+  }, 
+  work: { 
+    type: String, 
+    require: true
+}, 
+}) 
+
+const contactSchema = new mongoose.Schema({ 
+  name: { 
+      type: String, 
+      require: true
+  }, 
+  email: { 
+      type: String, 
+      require: true
+  }, 
+  phone: { 
+    type: String, 
+    require: true
+}, 
+message: { 
+  type: String, 
+  require: true
+}, 
+}) 
+
+const User = new mongoose.model("Attend", attendaceSchema) 
+const ContactUser = new mongoose.model("Contact", contactSchema) 
+  
+
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/', async (req, res) => {
-    try {
-      let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-          user: 'veer2238rajput@gmail.com',
-          pass: 'jeasqtbfommzmxya'
-        },
-      });
-  
-    
-  
-      let info = await transporter.sendMail({
-        from: 'veer2238rajput@gmail.com',
-        to: 'himanshu0409agraval@gmail.com',
-        subject: "thanks for registration",
-        text: "Hello world?",
-        html: `<p>Hi ${req.body.name},</p>
-                <p>Thank you for attending the lecture on ${req.body.date}. </p>
-                <p>Here are the details of your work:</p>
-                <p><strong>Work:</strong> ${req.body.work}</p>
-                <p>We appreciate your dedication and hard work!</p>
-                <img src="https://i.ibb.co/gyh8tYH/Untitled.png" alt="Thank You Image" style="max-width: 100%; height: auto;">
-                <p>Best regards,</p>
-                <p>V-Ex Tech Solution</p>`
-      
-      });
+// Connect to your Atlas cluster
 
-     
-  
-      console.log("Message sent: %s", info.messageId);
-  
-     // Respond with success
-     
-     res.status(200).json({ success: true, message: 'Email sent successfully' });
-    } catch (error) {
-      console.error('Attendance Error:', error);
-      res.status(500).json({ success: false, error: 'Failed to send email' });
-    }
-  });
+
+
+app.post('/', async (req, res) => {
+  const { name, date, work } = req.body;
+
+  // Check if the attendance record already exists
+  const existingRecord = await User.findOne({ name, date });
+
+  if (existingRecord) {
+    return res.status(400).json({ success: false, error: 'Your record already exists' });
+  }
+
+  try {
+    // If the record doesn't exist, create a new one
+    const result = await User.create({
+      name,
+      date,
+      work,
+    });
+
+    console.log(result);
+
+    // Send success response
+    res.status(200).json({ success: true, message: 'Attendance record added successfully' });
+  } catch (error) {
+    console.error('Attendance Error:', error);
+    res.status(500).json({ success: false, error: 'Failed to add attendance record' });
+  }
+});
 
 
   app.post('/contact', async (req, res) => {
+
+
+    const body = req.body;
+    const result = await ContactUser.create({
+      name:body.name,
+      email:body.email,
+      phone:body.phone,
+      message:body.message,
+
+    })
+  console.log(result)
     try {
       let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
