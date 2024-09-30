eb6b420a9f474a67685700703c58a6f3fe3a5981
veer2238 <veer2238rajput@gmail.com>
2024-09-30 21:54:58 +0530

    index config

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ import dotenv from "dotenv";
 import crypto from 'crypto';
 import nodemailer from 'nodemailer';
 import axios from 'axios'
-// import fs from 'fs'
-// import {PDFDocument, rgb} from 'pdf-lib'
+import Contacroute from './routes/Contact'
+
+
 
 
 
@@ -90,24 +91,7 @@ const passportSchema = new mongoose.Schema({
 
 
 
-const contactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: true,
-  },
-  email: {
-    type: String,
-    require: true,
-  },
-  phone: {
-    type: String,
-    require: true,
-  },
-  message: {
-    type: String,
-    require: true,
-  },
-});
+
 
 
 const certiSchema = new mongoose.Schema({
@@ -143,11 +127,13 @@ const certiSchema = new mongoose.Schema({
   const Certi = mongoose.model('Certificate', certiSchema);
 
 const User = mongoose.model("Attend", attendaceSchema);
-const ContactUser = mongoose.model("Contact", contactSchema);
+
 
 const Passport = mongoose.model('passport', passportSchema);
   
 app.use(cors());
+
+app.use('/',Contacroute)
 
 app.use(bodyParser.json());
 
@@ -331,82 +317,7 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.post("/contact", async (req, res) => {
-  const {name,email,phone,message} = req.body;
-try{
-  const exist =await ContactUser.findOne({email,message})
-  if(exist){
-    return res.json({success:false,error:'you have already enquired about the same issue'})
-  }
-  const result = await ContactUser.create({
-    name,
-    email,
-    phone,
-    message
-  });
 
-  console.log(result);
-
-  
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      cc: "himanshu0409agraval@gmail.com",
-      subject: "Thanks for Contacting at V-Ex Tech Solution",
-      html: `
-        <div style="background-color: #f8f8f8; padding: 20px; font-family: 'Arial', sans-serif; color: #333;">
-          <p style="font-size: 18px; color: #007BFF;">Hi ${name},</p>
-          <p style="font-size: 16px;">Thank you for contacting V-Ex Tech Solution. We have received your query and will get back to you soon.</p>
-    
-          <div style="margin: 20px 0; padding: 10px; background-color: #fff; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
-            <p style="font-size: 16px;">We have your contact details:</p>
-            <p style="font-size: 16px; margin-bottom: 5px;"><strong>Email:</strong> ${email}</p>
-            <p style="font-size: 16px; margin-bottom: 5px;"><strong>Phone:</strong> ${phone}</p>
-          </div>
-    
-          <img src="https://v-extechsolution.in/static/media/logo.b48612c02e688a28a62f.png" alt="Thank You Image" style="max-width: 100%; height: auto; margin-bottom: 20px;">
-    
-          <p style="font-size: 16px;">In the meantime, feel free to call us at <strong style="color: #007BFF;">+91 9664768292</strong>. We look forward to assisting you!</p>
-    
-          <p style="font-size: 16px; margin-top: 20px;">Best regards,</p>
-          <p style="font-size: 16px;">V-Ex Tech Solution</p>
-          <p style="font-size: 16px;">301, DHUN COMPLEX, ABOVE</p>
-          <div style="margin-top: 50px; color: #666;">
-          <a href="https://v-extechsolution.in" style="font-size: 16px; line-height: 1.5;">v-extechsolution.in</a><br>
-          <a href="mailto:veeragraval@v-extechsolution.in" style="font-size: 16px; line-height: 1.5;">veeragraval@v-extechsolution.in</a><br>
-          <a href="tel:9664768292" style="font-size: 16px; line-height: 1.5;">+91 9664768292</a>
-    <div style="margin-top: 50px;">
-          <a href="https://www.linkedin.com/company/v-ex-tech-software-company-in-vadodara/mycompany/" style="text-decoration: none; color: #333; padding:0 14px;"><img src="https://i.ibb.co/1MpdrG8/download-1.png" alt="download-1" border="0" style="width:15%"></a>
-          <a href="https://www.youtube.com/@Veer_Agraval" style="text-decoration: none; color: #333; padding:0 14px;"><img src="https://i.ibb.co/b60S7TZ/download.png" alt="download-1" border="0" style="width:15%"></a>
-          <a href="https://www.instagram.com/v_extech/?igshid=Zjc2ZTc4Nzk%3D" style="text-decoration: none; color: #333; padding:0 14px;"><img src="https://i.ibb.co/xYLHv49/download.jpg" alt="download-1" border="0" style="width:15%"></a>
-        </div>
-        </div>
-        <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">Dhun Complex-301, Above Riya Bridal, near Amritsari Kulcha, opp. pavan park society, Nizampura, Vadodara, Gujarat 390002</p>
-        </div>
-      `,
-    });
-
-  
-    
-
-    console.log("Message sent:", info.messageId);
-    res.json({success:true,message:' Thanks Your message has been sent'})
-
-  } catch (error) {
-    console.error("Contact Error:", error);
-    res
-      .status(500)
-      .json({ success: false, error: "Failed to send message" });
-  }
-});
 
 
 
---
+++ models/Contact.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    phone: {
+      type: String,
+      require: true,
+    },
+    message: {
+      type: String,
+      require: true,
+    },
+  })
+
+  const ContactUser = mongoose.model("Contact", contactSchema);
+  module.exports=ContactUser
