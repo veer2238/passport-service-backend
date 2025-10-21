993e09427fcc1bb65684793482570968f0a3b12d
“veer2238” <veer2238rajput@gmail.com>
2025-10-21 21:52:21 +0530

    index

--- routes/Contact.js
+++ routes/Contact.js
@@ -28,12 +28,14 @@ app.post("/contact", async (req, res) => {
   
     
       const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      });
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
   
       const info = await transporter.sendMail({
         from: process.env.EMAIL_USER,
